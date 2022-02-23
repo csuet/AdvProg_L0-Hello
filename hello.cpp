f3608e58747ff426baeba2a5ea0a6b7a332ea1e4
jackieTruongxyz <100253478+jackieTruongxyz@users.noreply.github.com>
2022-02-23 10:58:13 +0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include "hello.h"
 
-using namespace std;
-
-string printGameOver(){
+std::string printGameOver(){
 	// TODO: Return the required string
 	return "Game Over";
-}
-int main(){
-    cout<<printGameOver();
 }
