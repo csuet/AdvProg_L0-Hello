e176c44499d6577db7ae4b270f24270ec772f255
tuananhtran21020280 <100192298+tuananhtran21020280@users.noreply.github.com>
2022-02-23 12:21:15 +0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,9 @@
 
 std::string printGameOver(){
 	// TODO: Return the required string
+<<<<<<< HEAD
 	return "Game Over!";
+=======
+	return "Game Over!";
+>>>>>>> parent of 00b1378 (Update hello.cpp)
 }
